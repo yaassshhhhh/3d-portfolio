c04b754c7c49bd9029fb27f8a2606aefbd0a6e9d
yash mahajan <mahajanyash2054@gmail.com>
2026-02-05 01:02:56 +0530

    Fix team data: Restore Yash Mahajan and remove duplicates

--- techoverflow 3D portfolio/src/components/Overlay/TeamCarousel/teamData.js
+++ techoverflow 3D portfolio/src/components/Overlay/TeamCarousel/teamData.js
@@ -1,11 +1,11 @@
 // Team data
 const teamData = [
   {
-    name: "Aryan Vishal Patil",
-    image: "/images/aryan.jpg",
+    name: "Yash Mahajan",
+    image: "/images/yash_devfest.jpg",
     links: [
-      { title: "linkedin", link: "https://www.linkedin.com/in/aryan-patil-626a8828b/" },
-      { title: "github", link: "https://github.com/aryxett" },
+      { title: "linkedin", link: "https://www.linkedin.com/in/yash-mahajan-045380289/" },
+      { title: "github", link: "https://github.com/yaassshhhhh/3d-portfolio.git" },
       { title: "twitter", link: "https://t.me/tech_overflow" },
       { title: "portfolio", link: "https://t.me/tech_overflow" },
     ],
@@ -16,6 +16,30 @@ const teamData = [
     links: [
       { title: "linkedin", link: "https://www.linkedin.com/in/pranav-jadhav-20aa13211" },
       { title: "github", link: "https://github.com/Raj300512" },
+      { title: "twitter", link: "https://t.me/tech_overflow" },
+      { title: "portfolio", link: "https://t.me/tech_overflow" },
+    ],
+  },
+
+  {
+    name: "Anubhav",
+    image: "/images/zx2.PNG",
+    links: [
+      {
+        title: "linkedin",
+        link: "https://t.me/tech_overflow",
+      },
+      { title: "github", link: "https://t.me/tech_overflow" },
+      { title: "twitter", link: "https://t.me/tech_overflow" },
+      { title: "portfolio", link: "https://t.me/tech_overflow" },
+    ],
+  },
+  {
+    name: "Aryan Vishal Patil",
+    image: "/images/aryan.jpg",
+    links: [
+      { title: "linkedin", link: "https://www.linkedin.com/in/aryan-patil-626a8828b/" },
+      { title: "github", link: "https://github.com/aryxett" },
       { title: "twitter", link: "https://t.me/tech_overflow" },
       { title: "portfolio", link: "https://t.me/tech_overflow" },
     ],
