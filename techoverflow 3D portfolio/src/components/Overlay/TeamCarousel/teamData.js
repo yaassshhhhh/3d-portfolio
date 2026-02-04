f3b0689abb668ba1819a0af0592b7ce54f704682
Pranav  Arun Jadhav <130997742+Raj300512@users.noreply.github.com>
2026-02-04 21:26:33 +0530

    Update team: Revert Pranav photo, Add Aryan Vishal Patil

--- techoverflow 3D portfolio/src/components/Overlay/TeamCarousel/teamData.js
+++ techoverflow 3D portfolio/src/components/Overlay/TeamCarousel/teamData.js
@@ -11,11 +11,11 @@ const teamData = [
     ],
   },
   {
-    name: "DEV",
-    image: "/images/zx1.png",
+    name: "Pranav Arun Jadhav",
+    image: "/images/pranav_photo.jpg",
     links: [
-      { title: "linkedin", link: "https://t.me/tech_overflow" },
-      { title: "github", link: "https://t.me/tech_overflow" },
+      { title: "linkedin", link: "https://www.linkedin.com/in/pranav-jadhav-20aa13211" },
+      { title: "github", link: "https://github.com/Raj300512" },
       { title: "twitter", link: "https://t.me/tech_overflow" },
       { title: "portfolio", link: "https://t.me/tech_overflow" },
     ],
@@ -47,14 +47,11 @@ const teamData = [
     ],
   },
   {
-    name: "Aman",
-    image: "/images/zx3.PNG",
+    name: "Aryan Vishal Patil",
+    image: "/images/aryan.jpg",
     links: [
-      {
-        title: "linkedin",
-        link: "https://t.me/tech_overflow",
-      },
-      { title: "github", link: "https://t.me/tech_overflow" },
+      { title: "linkedin", link: "https://www.linkedin.com/in/aryan-patil-626a8828b/" },
+      { title: "github", link: "https://github.com/aryxett" },
       { title: "twitter", link: "https://t.me/tech_overflow" },
       { title: "portfolio", link: "https://t.me/tech_overflow" },
     ],
